613af28c84d78bc7ad9dbb3c8872194a73ad3c6b
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2025-10-15 19:31:55 +0530

    feat: added auth to main page

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { Header } from "../components/Common/Common";
+import { OAUTH_LOGIN_URL, useAuthContext } from "../utils/auth";
 
 function MainPage() {
+	const auth = useAuthContext();
+
+	useEffect(() => {
+		if (!auth.isAuthenticated) {
+			window.location.assign(OAUTH_LOGIN_URL);
+		}
+	}, []);
+
 	return <>
 		<Header
 			title="MetaKGP Maintainers' Dashboard"
